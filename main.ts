e5edff39afc5720dac6ec17a91bbb95dfd001522
dhcodms <113489433+dhcodms@users.noreply.github.com>
2022-09-21 09:49:34 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 let 나침반 = 0
+basic.showLeds(`
+    . . # . .
+    . # # . .
+    # # # # #
+    . . # . .
+    . . # . .
+    `)
+basic.pause(100)
 basic.forever(function () {
     나침반 = input.compassHeading()
     if (나침반 < 45) {
-        basic.showString("N")
+        basic.showArrow(ArrowNames.North)
     } else if (나침반 < 135) {
         basic.showString("W")
     } else if (나침반 < 225) {

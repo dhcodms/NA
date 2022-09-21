dbb6d0441da1feb9d430aeed8fd3838ed566e9b4
dhcodms <113489433+dhcodms@users.noreply.github.com>
2022-09-21 09:45:37 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -4,9 +4,9 @@ basic.forever(function () {
     if (나침반 < 45) {
         basic.showString("N")
     } else if (나침반 < 135) {
-        basic.showString("S")
-    } else if (나침반 < 225) {
         basic.showString("W")
+    } else if (나침반 < 225) {
+        basic.showString("S")
     } else if (나침반 < 315) {
         basic.showString("E")
     } else {
